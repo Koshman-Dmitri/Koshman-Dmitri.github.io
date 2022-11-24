99f6b32b909aa17fc1045a4d0f8636fbdd2ba0e9
Koshman-Dmitri <123456desher@rambler.ru>
2022-11-24 16:08:36 +0300

    Test commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Finally i`ve started React!
+          Test
         </p>
       </header>
     </div>
