ff6de486240a0d26b76e7a7b872227144d74aad5
Koshman-Dmitri <123456desher@rambler.ru>
2022-12-26 16:01:05 +0300

    input value

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -3,7 +3,11 @@ import '../Styles/userInput.css';
 export default function UserInput(props) {
     return (
         <div>
-            <input className="input" type="text" onChange={props.onInputChange} placeholder="Message" />
+            <input className="input"
+                   type="text"
+                   onChange={props.onInputChange}
+                   value={props.message}
+                   placeholder="Message" />
             <h2 className="message"><span className="count">{props.count}</span> + {props.message}</h2>
         </div>
     );
